1a7cff923d69e46ca1f18c305e3a8cacd4613ea8
Gorana Gojic <gorana.gojic.dev@gmail.com>
2020-10-16 19:50:52 +0200

    CHASE and DRIVE evaluation scripts restructured.

--- Eval_chase.py
+++ Eval_chase.py
@@ -1,95 +1,69 @@
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
+from keras.callbacks import  ModelCheckpoint
 from tensorflow import ConfigProto, Session
+
 config = ConfigProto()
 config.gpu_options.allow_growth = True
 sess = Session(config=config)
 
-import cv2
-import numpy as np
-import imageio
-from sklearn.metrics import recall_score, roc_auc_score, accuracy_score, confusion_matrix
-
-from keras.callbacks import  ModelCheckpoint
-import scipy.misc as mc
-import math
 from util import *
+from SA_UNet import SA_UNet
+
+
+# TODO modify the section above
+# =================================================================== #
+model_name = "Model/CHASE/SA_UNet.h5"
 data_location = './'
-testing_images_loc = data_location + 'CHASE/test/images/'
-testing_label_loc = data_location + 'CHASE/test/labels/'
+testing_images_loc = os.path.join(data_location, 'CHASE/test/images/')
+testing_label_loc = os.path.join(data_location, 'CHASE/test/labels/')
+fov_masks_loc = os.path.join(data_location, 'CHASE/test/masks')
+output_dir = 'CHASE/test/results'
+n_test_images = 8
+image_width, image_height = (960, 999)
+desired_size = 1008
+threshold = 0.5
+use_fov = True
+# =================================================================== #
 
-test_files = os.listdir(testing_images_loc)
-test_data = []
-test_label = []
-desired_size=1008
-for i in test_files:
-    im = imageio.imread(testing_images_loc + i)
-    label = imageio.imread(testing_label_loc + "Image_" + i.split('_')[1].split(".")[0] + "_1stHO.png", pilmode="L")
-    old_size = im.shape[:2]  # old_size is in (height, width) format
-    delta_w = desired_size - old_size[1]
-    delta_h = desired_size - old_size[0]
 
-    top, bottom = delta_h // 2, delta_h - (delta_h // 2)
-    left, right = delta_w // 2, delta_w - (delta_w // 2)
+def get_label_name(image_path):
+    return Path(image_path).stem + '_1stHO.png'
 
-    color = [0, 0, 0]
-    color2 = [0]
-    new_im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT,
-                                value=color)
 
-    new_label = cv2.copyMakeBorder(label, top, bottom, left, right, cv2.BORDER_CONSTANT,
-                                   value=color2)
+def get_mask_name(image_path):
+    return Path(image_path).stem + '.png'
 
-    test_data.append(cv2.resize(new_im, (desired_size, desired_size)))
-    temp = cv2.resize(new_label,
-                      (desired_size, desired_size))
-    _, temp = cv2.threshold(temp, 127, 255, cv2.THRESH_BINARY)
-    test_label.append(temp)
-test_data = np.array(test_data)
-test_label = np.array(test_label)
 
-x_test = test_data.astype('float32') / 255.
+print("> Predicting on CHASE dataset.")
 
-y_test = test_label.astype('float32') / 255.
-x_test = np.reshape(x_test, (len(x_test), desired_size, desired_size, 3))  # adapt this if using `channels_first` image data format
-y_test = np.reshape(y_test, (len(y_test), desired_size, desired_size, 1))  # adapt this if using `channels_first` im
-y_test=crop_to_shape(y_test,(len(y_test), 960, 999, 1))
+x_test, y_test = load_test_files(testing_images_loc, testing_label_loc, desired_size, get_label_name)
 
-from  SA_UNet import *
-model=SA_UNet(input_size=(desired_size,desired_size,3),start_neurons=16,lr=1e-4,keep_prob=1,block_size=1)
-model.summary()
-weight="Model/CHASE/SA_UNet.h5"
+model = SA_UNet(input_size=(desired_size, desired_size, 3), start_neurons=16, lr=1e-4, keep_prob=1, block_size=1)
 
-if os.path.isfile(weight): model.load_weights(weight)
+if os.path.isfile(model_name):
+    model.load_weights(model_name)
+    print(f"> Model {model_name} loaded for prediction.")
 
-model_checkpoint = ModelCheckpoint(weight, monitor='val_accuracy', verbose=1, save_best_only=False)
+model_checkpoint = ModelCheckpoint(model_name, monitor='val_accuracy', verbose=1, save_best_only=False)
 
 y_pred = model.predict(x_test)
-y_pred= crop_to_shape(y_pred,(20,960,999,1))
-y_pred_threshold = []
-i=0
-for y in y_pred:
+y_pred = crop_to_shape(y_pred,(n_test_images, image_width, image_height, 1))
 
-    _, temp = cv2.threshold(y, 0.5, 1, cv2.THRESH_BINARY)
-    y_pred_threshold.append(temp)
-    y = y * 255
-    cv2.imwrite('CHASE1/test/results/%d.png' % i, y)
-    i+=1
+# save predictions
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+for i, y in enumerate(y_pred):
+    _, temp = cv2.threshold(y, threshold, 1, cv2.THRESH_BINARY)
+    cv2.imwrite(os.path.join(output_dir, f"{i}.png"), y * 255)
+    cv2.imwrite(os.path.join(output_dir, f"{i}-thresholded.png"), temp * 255)
 y_test = list(np.ravel(y_test))
-y_pred_threshold = list(np.ravel(y_pred_threshold))
 
-tn, fp, fn, tp = confusion_matrix(y_test, y_pred_threshold).ravel()
+# load masks if you want statistics to be calculate just for inside fov
+all_masks_data = None
+if use_fov:
+    all_masks_data = np.ravel(load_mask_files(fov_masks_loc, testing_images_loc, get_mask_name))
 
-print('Accuracy:', accuracy_score(y_test, y_pred_threshold))
-
-print('Sensitivity:', recall_score(y_test, y_pred_threshold))
-
-print('Specificity', tn / (tn + fp))
-
-print('AUC:', roc_auc_score(y_test, list(np.ravel(y_pred))))
-print("F1:",2*tp/(2*tp+fn+fp))
-N=tn+tp+fn+fp
-S=(tp+fn)/N
-P=(tp+fp)/N
-print("MCC:",(tp/N-S*P)/math.sqrt(P*S*(1-S)*(1-P)))
+evaluate(y_test=y_test, y_pred=np.ravel(y_pred), threshold=threshold, mask_data=all_masks_data, use_fov=use_fov)
